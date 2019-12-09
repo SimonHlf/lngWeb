ffca4edc726eb44f4aff480edd4d7d1991187742
simon <simonHlf@163.com>
2019-12-09 17:02:18 +0800

    添加用户管理
    Signed-off-by: simon <simonHlf@163.com>

--- static/js/frame/js/common.js
+++ static/js/frame/js/common.js
@@ -14,6 +14,41 @@ layui.define(['form'],function(exports){
         vercode : function(){        	
         	var obj = this.getId("sessCode");
 			obj.src = "getVerify?"+Math.random()+100;
+		},
+		//获取角色list select/table
+		getRoleList : function(roleId,loadOpt){
+			var _this = this;
+			$.ajax({
+			    type:"get",
+				data : { roleId:roleId },
+			    dataType:"json",
+			    url:"/role/queryRole",
+			    success:function (json){
+			    	layer.closeAll('loading');
+					if(json.code == 200){
+						if(loadOpt == 'selLoad'){
+							_this.createRoleSel(json.datas);	
+						}else if(loadOpt == 'tableLoad'){
+							_this.loadRoleTabList(json.datas);
+						}
+					}else if(json.code == 1000){
+						layer.msg('服务器错误');
+					}else if(json.code == 50001){
+						layer.msg('暂无数据');
+					}else if(json.code == 50003){
+						layer.msg('数据已存在');
+					}
+			    }
+			});
+		},
+		//创建选择角色select
+		createRoleSel : function(list){
+			var str = '<option value="">请选择角色</option>';
+			for(var i=0;i<list.length;i++){
+				str += '<option value="'+ list[i].id +'">'+ list[i].depName +'</option>';
+			}
+			$('#roleSel').html(str);
+			form.render();
 		}
     };
     //输出接口
