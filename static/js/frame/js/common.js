2a4edd903fa5d21f7c5479a9bdfa1f7aa833e47a
simon <simonHlf@163.com>
2019-12-18 17:03:39 +0800

    添加贸易商模块
    Signed-off-by: simon <simonHlf@163.com>

--- static/js/frame/js/common.js
+++ static/js/frame/js/common.js
@@ -48,7 +48,38 @@ layui.define(['form','table'],function(exports){
 			}
 			$('#roleSel').html(str);
 			form.render();
-		}
+		},
+		//获取公司类型
+		getCompTypeSel : function(){
+			var _this = this;
+			layer.load();
+			$.ajax({
+			    type:"get",
+				data : { id:'' },
+			    dataType:"json",
+			    url:"/comType/queryComType",
+			    success:function (json){
+			    	layer.closeAll('loading');
+					if(json.code == 200){
+						_this.createCompTypeSel(json.datas);
+					}else if(json.code == 1000){
+						layer.msg('服务器错误');
+					}else if(json.code == 50001){
+						$('#compTypeSel').html('<option value="">请选择公司类型</option>');
+						form.render();
+					}
+			    }
+			});
+		},
+		//创建公司类型select
+		createCompTypeSel : function(list){
+			var str = '<option value="">请选择公司类型</option>';
+			for(var i=0;i<list.length;i++){
+				str += '<option value="'+ list[i].id +'">'+ list[i].name +'</option>';
+			}
+			$('#compTypeSel').html(str);
+			form.render();
+		},
     };
     //输出接口
     exports('common', obj);
